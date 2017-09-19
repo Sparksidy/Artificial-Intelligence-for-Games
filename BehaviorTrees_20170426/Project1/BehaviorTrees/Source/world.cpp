4dc677d0c18de9649731fd30e6d6ae480129212b
Siddharth Gupta <Siddharth Gupta>
2017-09-19 14:24:54 -0700

    ChangeBlue

--- BehaviorTrees_20170426/Project1/BehaviorTrees/Source/world.cpp
+++ BehaviorTrees_20170426/Project1/BehaviorTrees/Source/world.cpp
@@ -87,21 +87,25 @@ void World::Initialize( CMultiAnim *pMA, std::vector< CTiny* > *pv_pChars, CSoun
 #else
 		g_trees.Initialize();		// initialize behavior tree
 
-	for( int i=0; i<10; i++ )
+	for( int i=0; i<1; i++ )
 	{
 		//Create game objects
 		char name[10] = "BTAgent";
 		sprintf( name, "%s%d", name, i );
 		GameObject* npc = new GameObject( g_database.GetNewObjectID(), OBJECT_NPC, name );
+		
 		D3DXVECTOR3 pos(0.0f, 0.0f, 0.0f);
 		pos.x = g_random.RangeFloat();
-		pos.z = g_random.RangeFloat();
+		pos.z = g_random.RangeFloat();\
+
 		npc->CreateBody( 100, pos );
+		
+
 		npc->CreateMovement();
-		npc->CreateTiny( pMA, pv_pChars, pSM, dTimeCurrent, 1.0f, 1.0f, 1.0f );	//Color if needed
+		npc->CreateTiny( pMA, pv_pChars, pSM, dTimeCurrent, 0.0f, 0.0f, 1.0f );	//Color if needed
 		g_database.Store( *npc );
 
-		g_trees.Register(name, "Example");							// register agent to behavior tree
+		g_trees.Register(name, "AsleepAgentTree");							// register agent to behavior tree
 		g_trees.GetAgentData(name).InitialTinyBlackBoard(npc);		// initialize local blackboard for each tiny
 	}
 
